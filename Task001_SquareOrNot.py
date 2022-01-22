e17fc9b85929dfc647ee554ebf1044ae3fff0387
Artem <kardan97@yandex.ru>
2022-01-22 17:29:15 +0700

    Task 1 Check square or not solved

---
+++ Task001_SquareOrNot.py
@@ -0,0 +1,13 @@
+# //  1. По двум заданным числам проверять является ли первое квадратом второго
+
+def IsSquare(value_1, value_2):
+    if value_1 == value_2 ** 2:
+        return True
+    else: 
+        return False
+
+print('Введите первое число')
+number_1 = int(input())
+print('Введите второе число')
+number_2 = int(input())
+print(IsSquare(number_1, number_2))
