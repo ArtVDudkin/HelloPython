cc6e51bcdef9bd19428702bbea3c971cfaab92a2
Artem <kardan97@yandex.ru>
2022-01-22 19:05:21 +0700

    Task 9 Show last digit solved

--- Task009_ShowLastDigit.py
+++ Task009_ShowLastDigit.py
@@ -1,12 +1,12 @@
-# 9. Показать вторую цифру трёхзначного числа
+# 9. Показать последнюю цифру трёхзначного числа
 
 import random
 
 
-def SecondDigit(value):
-    return value // 10 % 10
+def LastDigit(value):
+    return value % 10
 
 
 num = random.randint(100, 999)
 print(num)
-print(SecondDigit(num))
+print(LastDigit(num))
