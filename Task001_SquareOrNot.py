5efbdba2b10ef995e9641d1e89db4964eb4524fd
Artem <kardan97@yandex.ru>
2022-02-05 21:36:42 +0700

    Task 1 resolved after task list was edited

--- Task001_SquareOrNot.py
+++ Task001_SquareOrNot.py
@@ -1,10 +1,7 @@
 # 1. По двум заданным числам проверять является ли первое квадратом второго
 
 def IsSquare(value_1, value_2):
-    if value_1 == value_2 ** 2:
-        return True
-    else: 
-        return False
+    return value_1 == value_2 ** 2
 
 print('Введите первое число')
 number_1 = int(input())
